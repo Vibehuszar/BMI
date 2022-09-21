0deef3b51fb2c17f16fb5dbb04a95263d642d02d
Vibehuszar <Vibehuszar@users.noreply.github.com>
2022-09-21 08:30:46 +0000

    deploy: 6aef229507294a8075d60207161894df2c81d664

--- java.js
+++ java.js
@@ -12,26 +12,32 @@ function range(){
     if(bmi <=  18.5){
         
         document.getElementById('elso').style.backgroundColor = 'orange';
+        document.getElementById('elso').style.color = 'white';
     }
     else if(bmi <= 24.9){
         document.getElementById('masodik').style.backgroundColor = 'orange';
+        document.getElementById('masodik').style.color = 'white';
     }
     else if(bmi <= 29.9){
         document.getElementById('harmadik').style.backgroundColor = 'orange';
+        document.getElementById('harmadik').style.color = 'white';
     }
     else if(bmi <= 34.9){
         document.getElementById('negyedik').style.backgroundColor = 'orange';
+        document.getElementById('negyedik').style.color = 'white';
     }
     else if(bmi <= 39.9){
         document.getElementById('otodik').style.backgroundColor = 'orange';
+        document.getElementById('otodik').style.color = 'white';
     }
     else{
         document.getElementById('hatodik').style.backgroundColor = 'orange';
+        document.getElementById('hatodik').style.color = 'white';
     }
 }
 
 function idealis(){
-    
+
 }
 
 function init(){
